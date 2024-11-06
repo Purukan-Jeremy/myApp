9447730e5e1a5e45ed9552f74d43ae71cd67b82f
Purukan-Jeremy <s22210214@student.unklab.ac.id>
2024-11-06 09:58:15 +0800

    hpus tu sir d sruh

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ import Footer from './components/Footer'
 const App = () => {
   return (
   <div>
-  <meta charSet="utf-8" />
-  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width,initial-scale=1" />
-  <title>Cube Template</title>
-  <link rel="stylesheet" href="css/style.css" />
   <div className="body-wrap">
     <Header/>
     <main className="site-content">
